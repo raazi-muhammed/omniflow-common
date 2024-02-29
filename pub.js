f1a3ab0cbbd4c38ebf9b34d3a3d33f77dc4a8af6
Raazi Muhammed K <raazi6163@gmail.com>
2024-02-29 18:42:53 +0530

    updates

--- pub.js
+++ pub.js
@@ -1,10 +1,8 @@
-// pub.js
 const { execSync } = require("child_process");
 const argv = require("yargs").argv;
 
 const commitMessage = argv.m || "updates";
 
-// Run the git commit command with the specified commit message
 execSync(
     `git add . && git commit -m "${commitMessage}"  && npm version patch && npm run build && npm publish && git push`,
     { stdio: "inherit" }
