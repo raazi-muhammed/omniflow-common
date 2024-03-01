e9acacc99b47038de0c0127a321f5c52e362b144
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-01 10:11:09 +0530

    updates

--- src/lib/adapt-request.ts
+++ src/lib/adapt-request.ts
@@ -1,5 +1,25 @@
 import { Request } from "express";
 
+type UserType = {
+    _id: string;
+    email: string;
+    name: string;
+    username: string;
+    password: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+    iat: number;
+};
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserType;
+        }
+    }
+}
+
 export function adaptRequest(req: Request) {
     return Object.freeze({
         path: req.path,
@@ -9,6 +29,7 @@ export function adaptRequest(req: Request) {
         body: req.body,
         headers: req.headers,
         cookies: req.cookies,
+        currentUser: req.currentUser,
     });
 }
 
