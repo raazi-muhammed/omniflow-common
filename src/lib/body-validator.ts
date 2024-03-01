e9ac92143ddf20affbac7a0315ce260070710ad1
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-01 12:01:03 +0530

    updates

--- src/lib/body-validator.ts
+++ src/lib/body-validator.ts
@@ -1,7 +1,7 @@
 export function validateBody(obj: Object, keys: string[]) {
     for (let i = 0; i < keys.length; i++) {
         // @ts-ignore
-        if (!obj[keys[i]]) {
+        if (obj[keys[i]] === undefined || obj[keys[i]] === null) {
             throw new Error(`Invalid data ${keys[i]}`);
         }
     }
