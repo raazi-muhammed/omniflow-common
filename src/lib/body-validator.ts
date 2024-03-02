277f8c750eda889228516073eee878e2eb2b9c6e
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-03 00:28:27 +0530

    updates

--- src/lib/body-validator.ts
+++ src/lib/body-validator.ts
@@ -1,8 +1,10 @@
+import { BadRequestError } from "./error-handler";
+
 export function validateBody(obj: Object, keys: string[]) {
     for (let i = 0; i < keys.length; i++) {
         // @ts-ignore
         if (obj[keys[i]] === undefined || obj[keys[i]] === null) {
-            throw new Error(`Invalid data ${keys[i]}`);
+            throw new BadRequestError(`Invalid data ${keys[i]}`);
         }
     }
     return true;
--- src/lib/error-handler.ts
+++ src/lib/error-handler.ts
@@ -5,3 +5,33 @@ export class ErrorHandler extends Error {
         this.statusCode = statusCode;
     }
 }
+
+export class BadRequestError extends ErrorHandler {
+    constructor(message: string = "Bad request") {
+        super(message, 400);
+    }
+}
+
+export class ConflictError extends ErrorHandler {
+    constructor(message: string = "Conflict error") {
+        super(message, 409);
+    }
+}
+
+export class InternalServerError extends ErrorHandler {
+    constructor(message: string = "Internal Server Error") {
+        super(message, 500);
+    }
+}
+
+export class NotFoundError extends ErrorHandler {
+    constructor(message: string = "Not found") {
+        super(message, 404);
+    }
+}
+
+export class UnauthorizedError extends ErrorHandler {
+    constructor(message: string = "Unauthorized") {
+        super(message, 401);
+    }
+}
