d37e5d4476caac03c882d63676fb52cba55ee149
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-04 04:07:48 +0530

    updates

--- src/lib/express-response.ts
+++ src/lib/express-response.ts
@@ -1,6 +1,6 @@
 import { IResponse } from "../interfaces/response.interface.js";
 
-export class ReposeCreator {
+export class ResponseCreator {
     private data?: Object | string;
     private statusCode: number = 200;
     private headers?: Object;
